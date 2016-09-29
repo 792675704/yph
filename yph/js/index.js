c7f186789e8c45201ab89adbfe84e5f7859996fd
792675704 <792675704@qq.com>
2016-09-29 17:09:28 +0800

    新建优品惠

---
+++ yph/js/index.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Administrator
+* @Date:   2016-09-28 21:20:09
+* @Last Modified by:   Administrator
+* @Last Modified time: 2016-09-28 21:20:26
+*/
+
+'use strict';
+$(function(){
+	
+})
